c98db60156bc258a578496c2f145bb2558905b0a
Kevin Velasco <37220428+xkevin190@users.noreply.github.com>
2024-12-13 22:49:42 +0100

    new changes

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,26 @@
 module.exports = {
   preset: 'react-native',
-  transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest",
-  },
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
-  globals: {
-    "ts-jest": {
-      useESM: true,
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
-
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/styles.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/**/*.spec.{ts,tsx}',
+    '!<rootDir>/src/**/*.test.{ts,tsx}',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|react-redux|@react-native-community|@testing-library|rn-secure-storage)',
+  ],
 };
